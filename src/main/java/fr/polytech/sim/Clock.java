d36f51c2ff2096f999b5d4e7c25e7d53a8b8e789
Luc <lucphilippe.brun@gmail.com>
2024-03-29 10:27:59 +0100

    question 3

--- src/main/java/fr/polytech/sim/Clock.java
+++ src/main/java/fr/polytech/sim/Clock.java
@@ -6,12 +6,26 @@ import java.util.Random;
  * A clock used to synchronize simulations.
  */
 public class Clock {
-    private final int time = new Random().nextInt(25);
 
+    private static Clock instance;
+
+    // Rend le constructeur privé pour que personne ne puisse l'appeler
+    private Clock() {}
+
+    // Méthode statique pour récupérer l'instance unique
+    public static Clock getInstance() {
+        if (instance == null) {
+            instance = new Clock();
+        }
+        return instance;
+    }
+    private final int time = new Random().nextInt(25);
     /**
      * Random integer between 0 and 24 inclusive.
      */
     public int getTime() {
         return this.time;
     }
+
+
 }
