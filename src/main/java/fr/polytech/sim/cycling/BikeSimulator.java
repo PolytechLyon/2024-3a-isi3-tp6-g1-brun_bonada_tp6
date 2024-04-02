acd07ac28fb55c6261cf63f4a5fc7653f8c11894
Nathan BONADA <nathan.bonada@gmail.com>
2024-04-02 14:47:03 +0200

    Exo 8 et 9

--- src/main/java/fr/polytech/sim/cycling/BikeSimulator.java
+++ src/main/java/fr/polytech/sim/cycling/BikeSimulator.java
@@ -3,6 +3,9 @@ package fr.polytech.sim.cycling;
 import fr.polytech.sim.Simulation;
 import fr.polytech.sim.log.FileLogger;
 import fr.polytech.sim.log.Logger;
+import fr.polytech.sim.utils.Context;
+
+import java.util.Iterator;
 
 /**
  * Bike simulation.
@@ -11,8 +14,14 @@ public class BikeSimulator implements Simulation {
     private final Logger logger = new FileLogger("BikeSimulator");
 
     public void run() {
-        Bike bike = new SimpleBike();
-        this.logger.log("Bike's speed %.2f Km/h.", bike.getVelocity());
-        this.logger.log("Bike's mass %.2f Kg.", bike.getMass());
+
+        Iterator<Bike> bikes = Context.injectAll(Bike.class);
+        while (bikes.hasNext()) {
+            Bike bike = bikes.next();
+            System.out.println("Simulating " + bike.getClass().getSimpleName());
+            this.logger.log("Bike's speed %.2f Km/h.", bike.getVelocity());
+            this.logger.log("Bike's mass %.2f Kg.", bike.getMass());
+        }
+
     }
 }
